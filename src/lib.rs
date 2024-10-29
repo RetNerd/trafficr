9db75aed31cd348eebde819c11f2f188f1dcbc50
Marton Racz <raczmarton292@gmail.com>
2024-10-29 13:09:42 +0100

    reboot

--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod proxy;
-
-pub use proxy::proxy::Proxy;
-
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod proxy;
-use proxy::proxy::Proxy;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proxy = Proxy::new().await?;
-    proxy.start().await?;
-    Ok(())
-}
